9131bdaabb7804ea82ec4f0a9e41aa6a49fda9e1
henrychanflag <henry.yk.chan@polyu.edu.hk>
2021-03-08 12:10:13 +0800

    R2

--- Heart_simulator_control.ino
+++ Heart_simulator_control.ino
@@ -1,10 +1,10 @@
-// Version: 1.4
+// Version: 2
 // By : Henry Chan
 // A motor control with flow sensor programme for Heart Simulator
 // Two outputs: one is PWM at 490Hz and one is a 50% duty-cycle variable frequency output
 // PWM & Pulse output will be activated by a switch
-// Flow sensor counter is detected by ext. interrupt input; overall number of counts is added & reset in FLOW_TIMER
-// Display is refreshed regularly inside the timer interrupt to ensure no display corruption every timer period (500ms)
+// Flow sensor counter is detected by ext. interrupt input; overall number of counts is added & reset in Timer2
+// Display is refreshed regularly inside the timer interrupt to ensure no display corruption every timer period (500ms) using NeoTimer
 // A 4 lines x 20 characters LCD module is attached using I2C interface
 // LED is to show the output status
 // A temp sensor detection is available
@@ -22,23 +22,24 @@
 #define OUTPUT_LED 12 // Output enable LED
 #define OUTPUT_DET 5  // Output enable detection
 #define FLOW_IN0 3    // FLOW sensor input1 as EXT INT
-#define FLOW_TIMER 500  // FLOW_TIMER in milli-seconds
-#define FLOW_TIMER_END 4 // FLOW_TIMER_END to calculate the flow counts once
+#define DISPLAY_TIMER 500  // DISPLAY_TIMER in milli-seconds
+#define debug 2
 
 // Set the LCD I2C address, lines and rows
 LiquidCrystal_I2C lcd(0x27,20,4); //Address: 0x27 for PCF8754; 0x3F for PCF8754A
-Neotimer mytimer = Neotimer(FLOW_TIMER); // Set timer interval
+Neotimer mytimer = Neotimer(DISPLAY_TIMER); // Set timer interval
 
-byte index = 0;
 int PWM_val = 0;    // PWM variable
 int PULSE_val = 0;  // Pulse variable
 int temp = 25;      // temp1 value
 int pulse_half_period = 300;  //Pulse output half period
 boolean pulse_output = HIGH;  //PULSE_OUTPUT port value
 int FLOW_count = 0; // Flow rate count
-float rate = 0;       //Flow rate
+//float rate = 0;       //Flow rate
+int rate = 0;
 int timer_flag = 0;
-byte customChar[] = {0x06,0x09,0x09,0x06,0x00,0x00,0x00,0x00}; //Character for degree symbol
+int display_timer_flag = 0;
+byte degree[] = {0x06,0x09,0x09,0x06,0x00,0x00,0x00,0x00}; //Character for degree symbol
 byte heart[] = {0x0A,0x1F,0x1F,0x1F,0x1F,0x0E,0x04,0x00};// Heart symbol
 byte flash[] = {0x00,0x00,0x08,0x15,0x02,0x00,0x00,0x00}; // Flash symbol
 byte danger[]= {0x06,0x0F,0x0F,0x0F,0x06,0x00,0x06,0x06}; // Danger symbol
@@ -48,10 +49,27 @@ void counting() {
   FLOW_count ++;
 }
 
-// Calculate the flow rate
-void flow_rate() {
-  //Calculate the flow rate per timer interrupt
-  timer_flag++;
+// Routine to display
+void display_rate() {  
+  //Display the icon here
+  //digitalWrite(debug,HIGH);
+  if (digitalRead(OUTPUT_DET) == 0){
+      if (display_timer_flag == 0){
+        lcd.setCursor(11,1);
+        lcd.write(1);
+        display_timer_flag++;
+      }
+      else{
+        lcd.setCursor(11,1);
+        lcd.write(2);
+        display_timer_flag++;
+      }
+      if (display_timer_flag ==2) display_timer_flag =0; //reset the flag
+    }
+    else {
+      lcd.setCursor(11,1);
+      lcd.print(' ');   
+    }
   
   // Display all the values at timer interal
   lcd.setCursor(0, 2);
@@ -74,14 +92,23 @@ void flow_rate() {
     lcd.setCursor(11,3);
     lcd.print(' ');
   }
+  //digitalWrite(debug,LOW);
+}
+
+ISR(TIMER2_COMPA_vect)      //timer2 interrupt 33Hz
+{
+  //digitalWrite(debug,HIGH);
+  timer_flag++;
   
-  // Calculate the flow count every 4 timer periods
-  if (timer_flag == FLOW_TIMER_END){
-  Serial.println(FLOW_count);
-  rate = 62*FLOW_count/1000;
-  timer_flag =0;  //reset timer_flag 
+  //Collect the flow count
+  //Count = 120 @ 2sec
+  if (timer_flag == 121){
+  digitalWrite(debug, digitalRead(debug) ^ 1);  //Turns debug IO ON and OFF
+  rate = FLOW_count;
   FLOW_count = 0; //reset the counter
   }
+  if (timer_flag > 121) timer_flag = 0; //Reset timer_flag
+  //digitalWrite(debug,LOW);
 }
 
 void setup() {
@@ -90,21 +117,39 @@ void setup() {
   pinMode(FLOW_IN0, INPUT_PULLUP);
   pinMode(OUTPUT_DET, INPUT_PULLUP);
   pinMode(OUTPUT_LED, OUTPUT);
+  pinMode(debug, OUTPUT);
 
   //initate IO ports
   digitalWrite(PWM_OUT, HIGH);
   digitalWrite(PULSE_OUT, HIGH);
   digitalWrite(OUTPUT_LED, LOW);
-    
+  digitalWrite(debug, LOW);
+
+// initialize timer2 
+  noInterrupts();           // disable all interrupts
+  TCCR2A = 0;// set entire TCCR2A register to 0
+  TCCR2B = 0;// same for TCCR2B
+  TCNT2  = 0;//initialize counter value to 0
+  // set compare match register for 8khz increments
+  // Value = 16M/desired frequency/Prescaler
+  OCR2A = 255;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set CS22 bit for 64 prescaler
+  TCCR2B |= (1 << CS22)|(1 << CS21)|(1<<CS20);   
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
+  interrupts();             // enable all interrupts
+
   // init LCD
   lcd.begin();
   
-  lcd.createChar(0, customChar);
+  lcd.createChar(0, degree);
   lcd.createChar(1, heart);
   lcd.createChar(2, flash);
   lcd.createChar(3, danger);
   
-    lcd.setCursor(0,0);
+  lcd.setCursor(0,0);
   // Print a message to the LCD.
   lcd.print("  HEART SIMULATOR");
   lcd.setCursor(0,1);
@@ -117,38 +162,20 @@ void setup() {
 
 void loop() {
   if(mytimer.repeat()){
-    flow_rate();
-    if (digitalRead(OUTPUT_DET) == 0){
-      if (index == 0){
-        lcd.setCursor(11,1);
-        lcd.write(1);
-        index++;
-      }
-      else{
-        lcd.setCursor(11,1);
-        lcd.write(2);
-        index++;
-      }
-      if (index ==2) index =0; //reset the index
-    }
-    else {
-      lcd.setCursor(11,1);
-      lcd.print(' ');   
-    }
+    display_rate();
   }
- 
   attachInterrupt(digitalPinToInterrupt(FLOW_IN0), counting, FALLING); //Enable the interrupt for flow detection
   temp = analogRead(TEMP_IN0)*50/1024*10;   // read temp sensor1
   PWM_val = analogRead(PWM_PIN);  // read PWM VR input
   PULSE_val = analogRead(PULSE_PIN); // read Pulse VR input
 
   //Debug values
-  Serial.print("PWM = "); 
-  Serial.print(PWM_val);
-  Serial.print(" PULSE = ");
-  Serial.print(PULSE_val);
-  Serial.print(" TEMP = "); 
-  Serial.println(temp);
+  //Serial.print("PWM = "); 
+  //Serial.print(PWM_val);
+  //Serial.print(" PULSE = ");
+  //Serial.print(PULSE_val);
+  //Serial.print(" TEMP = "); 
+  //Serial.println(temp);
 
   // Give the pulse output at 50% duty cycle
   // Map to min 0.667Hz (=40ppm,Period=1.5s) to max 3Hz (=180ppm,Period=0.333s)
@@ -165,8 +192,6 @@ void loop() {
     analogWrite(PWM_OUT, map(PWM_val,0,1023,0,255));  // write PWM value to PWM port, highest speed when output = 0
     analogWrite(PULSE_OUT,map(PWM_val,0,1023,0,255)); // write PWM value to PULSE_OUT port, highest speed when output = 0
     delay(pulse_half_period); 
-    //PWM_val = analogRead(PWM_PIN);  // read PWM VR input
-    //PULSE_val = analogRead(PULSE_PIN); // read Pulse VR input
     digitalWrite(PULSE_OUT, HIGH); // Write high to the PULSE PORT to stop in the half cycle
     delay(pulse_half_period);
   }
